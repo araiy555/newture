a21a5f839409b754900f2fb49696499365eaa5e8
新井耀介 <k016c1200@it-neec.jp>
2021-04-13 22:41:15 +0900

    commit

--- copySettingsAndPrivateFiles.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-
-console.log('Copying settings and private file if necessary');
-
-fs.copySync('.env.settings.sample', '.env.settings', { overwrite: false });
-
-fs.copySync('.env.private.sample', '.env.private', { overwrite: false });
