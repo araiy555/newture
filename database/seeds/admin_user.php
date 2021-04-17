ad56d889ca22a6fb3eb3389c419077809aa02de9
新井耀介 <k016c1200@it-neec.jp>
2021-04-18 04:21:40 +0900

    、

--- database/seeds/admin_user.php
+++ database/seeds/admin_user.php
@@ -3,8 +3,6 @@
 use Illuminate\Database\Seeder;
 use Encore\Admin\Auth\Database\Administrator;
 use Encore\Admin\Auth\Database\Role;
-use Encore\Admin\Auth\Database\Permission;
-use Encore\Admin\Auth\Database\Menu;
 
 class admin_user extends Seeder
 {
@@ -15,18 +13,13 @@ class admin_user extends Seeder
      */
     public function run()
     {
-        /**
-         * Run the database seeds.
-         *
-         * @return void
-         */
 
         // create a user.
         Administrator::truncate();
         Administrator::create([
             'username' => 'araiy',
-            'password' => bcrypt('Aq!ws2de3fr4'),
-            'name' => 'Administrator',
+            'password' => bcrypt('Aq!sw2de3fr4'),
+            'name'     => 'Administrator',
         ]);
 
         // create a role.
@@ -39,98 +32,21 @@ class admin_user extends Seeder
         // add role to user.
         Administrator::first()->roles()->save(Role::first());
 
-        //create a permission
-        Permission::truncate();
-        Permission::insert([
-            [
-                'name' => 'All permission',
-                'slug' => '*',
-                'http_method' => '',
-                'http_path' => '*',
-            ],
-            [
-                'name' => 'Dashboard',
-                'slug' => 'dashboard',
-                'http_method' => 'GET',
-                'http_path' => '/',
-            ],
-            [
-                'name' => 'Login',
-                'slug' => 'auth.login',
-                'http_method' => '',
-                'http_path' => "/auth/login\r\n/auth/logout",
-            ],
-            [
-                'name' => 'User setting',
-                'slug' => 'auth.setting',
-                'http_method' => 'GET,PUT',
-                'http_path' => '/auth/setting',
-            ],
-            [
-                'name' => 'Auth management',
-                'slug' => 'auth.management',
-                'http_method' => '',
-                'http_path' => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs",
-            ],
+        // 別のユーザー(例:staff）やロールを初期状態で追加しておく場合
+        Administrator::create([
+            'username' => 'staff',
+            'password' => bcrypt('staff'),
+            'name'     => 'staff',
         ]);
 
-        Role::first()->permissions()->save(Permission::first());
-
-        // add default menus.
-        Menu::truncate();
-        Menu::insert([
-            [
-                'parent_id' => 0,
-                'order' => 1,
-                'title' => 'Dashboard',
-                'icon' => 'fa-bar-chart',
-                'uri' => '/',
-            ],
-            [
-                'parent_id' => 0,
-                'order' => 2,
-                'title' => 'Admin',
-                'icon' => 'fa-tasks',
-                'uri' => '',
-            ],
-            [
-                'parent_id' => 2,
-                'order' => 3,
-                'title' => 'Users',
-                'icon' => 'fa-users',
-                'uri' => 'auth/users',
-            ],
-            [
-                'parent_id' => 2,
-                'order' => 4,
-                'title' => 'Roles',
-                'icon' => 'fa-user',
-                'uri' => 'auth/roles',
-            ],
-            [
-                'parent_id' => 2,
-                'order' => 5,
-                'title' => 'Permission',
-                'icon' => 'fa-ban',
-                'uri' => 'auth/permissions',
-            ],
-            [
-                'parent_id' => 2,
-                'order' => 6,
-                'title' => 'Menu',
-                'icon' => 'fa-bars',
-                'uri' => 'auth/menu',
-            ],
-            [
-                'parent_id' => 2,
-                'order' => 7,
-                'title' => 'Operation log',
-                'icon' => 'fa-history',
-                'uri' => 'auth/logs',
-            ],
+        // create a role.
+        Role::create([
+            'name' => 'staff',
+            'slug' => 'staff',
         ]);
 
-        // add role to menu.
-        Menu::find(2)->roles()->save(Role::first());
+        // add role to user.
+        Administrator::find(2)->roles()->save(Role::find(2));
+
     }
 }
