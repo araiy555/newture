8c569be3671823fabbf5f8d02a183f41d4c62cc2
新井耀介 <k016c1200@it-neec.jp>
2021-04-18 03:40:37 +0900

    管理機能

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Netflix</title>
+    <title>NetTvStation</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
 
@@ -304,17 +304,20 @@
             <a id="logo" href="#home"><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true" alt="Logo Image"></a>
         </div>
         <nav class="main-nav">
-            <a href="#home">Home</a>
-            <a href="#tvShows">TV Shows</a>
-            <a href="#movies">Movies</a>
-            <a href="#originals">Originals</a>
-            <a href="#">Recently Added</a>
-            <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">Portfolio</a>
+            <a href="#home">ホーム</a>
+            <a href="#tvShows">テレビ番組</a>
+            <a href="#movies">映画</a>
+            <a href="#originals">オリジナル</a>
+            <a href="#">音楽</a>
+            <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">漫画</a>
+            <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">アニメ</a>
+            <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">教育</a>
         </nav>
         <nav class="sub-nav">
             <a href="#"><i class="fas fa-search sub-nav-logo"></i></a>
             <a href="#"><i class="fas fa-bell sub-nav-logo"></i></a>
-            <a href="#">Account</a>
+            <a href="#">新規登録</a>
+            <a href="#">ログイン</a>
         </nav>
     </header>
     <!-- END OF HEADER -->
