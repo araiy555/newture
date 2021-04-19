54a55d643309c5b2a6186b4a81054e589880e04d
新井耀介 <k016c1200@it-neec.jp>
2021-04-19 12:46:58 +0900

    てｓｔ

--- database/seeds/admin_user.php
+++ database/seeds/admin_user.php
@@ -1,5 +1,7 @@
 <?php
 
+use Encore\Admin\Auth\Database\Menu;
+use Encore\Admin\Auth\Database\Permission;
 use Illuminate\Database\Seeder;
 use Encore\Admin\Auth\Database\Administrator;
 use Encore\Admin\Auth\Database\Role;
@@ -13,40 +15,115 @@ class admin_user extends Seeder
      */
     public function run()
     {
-
-        // create a user.
-        Administrator::truncate();
-        Administrator::create([
+        DB::table('admin_users')->insert([
             'username' => 'araiy',
             'password' => bcrypt('Aq!sw2de3fr4'),
             'name'     => 'Administrator',
         ]);
 
-        // create a role.
-        Role::truncate();
-        Role::create([
+        DB::table('admin_roles')->insert([
             'name' => 'Administrator',
-            'slug' => 'administrator',
+            'slug'     => 'administrator',
         ]);
 
-        // add role to user.
-        Administrator::first()->roles()->save(Role::first());
-
-        // 別のユーザー(例:staff）やロールを初期状態で追加しておく場合
-        Administrator::create([
-            'username' => 'staff',
-            'password' => bcrypt('staff'),
-            'name'     => 'staff',
+        DB::table('admin_role_users')->insert([
+            'role_id' => 1,
+            'user_id' => 1,
         ]);
 
-        // create a role.
-        Role::create([
-            'name' => 'staff',
-            'slug' => 'staff',
+        DB::table('admin_role_permissions')->insert([
+            'role_id' => 1,
+            'permission_id' => 1,
         ]);
 
-        // add role to user.
-        Administrator::find(1)->roles()->save(Role::find(1));
+        DB::table('admin_role_menu')->insert([
+            'role_id' => 1,
+            'menu_id' => 2,
+        ]);
+
+        DB::table('admin_permissions')->insert([
+            [
+                'name'        => 'All permission',
+                'slug'        => '*',
+                'http_method' => '',
+                'http_path'   => '*',
+            ],
+            [
+                'name'        => 'Dashboard',
+                'slug'        => 'dashboard',
+                'http_method' => 'GET',
+                'http_path'   => '/',
+            ],
+            [
+                'name'        => 'Login',
+                'slug'        => 'auth.login',
+                'http_method' => '',
+                'http_path'   => "/auth/login\r\n/auth/logout",
+            ],
+            [
+                'name'        => 'User setting',
+                'slug'        => 'auth.setting',
+                'http_method' => 'GET,PUT',
+                'http_path'   => '/auth/setting',
+            ],
+            [
+                'name'        => 'Auth management',
+                'slug'        => 'auth.management',
+                'http_method' => '',
+                'http_path'   => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs",
+            ],
+        ]);
+        DB::table('admin_menu')->insert([
+            [
+                'parent_id' => 0,
+                'order'     => 1,
+                'title'     => 'Dashboard',
+                'icon'      => 'fa-bar-chart',
+                'uri'       => '/',
+            ],
+            [
+                'parent_id' => 0,
+                'order'     => 2,
+                'title'     => 'Admin',
+                'icon'      => 'fa-tasks',
+                'uri'       => '',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 3,
+                'title'     => 'Users',
+                'icon'      => 'fa-users',
+                'uri'       => 'auth/users',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 4,
+                'title'     => 'Roles',
+                'icon'      => 'fa-user',
+                'uri'       => 'auth/roles',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 5,
+                'title'     => 'Permission',
+                'icon'      => 'fa-ban',
+                'uri'       => 'auth/permissions',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 6,
+                'title'     => 'Menu',
+                'icon'      => 'fa-bars',
+                'uri'       => 'auth/menu',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 7,
+                'title'     => 'Operation log',
+                'icon'      => 'fa-history',
+                'uri'       => 'auth/logs',
+            ],
+        ]);
 
     }
 }
